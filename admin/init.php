4c4390b8342a390e82b31cc806833352812b59b3
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-07-29 19:26:30 +0300

    add description and benefits init php file

--- admin/init.php
+++ admin/init.php
@@ -1,6 +1,10 @@
 <?php 
 
-$tbl = 'includes/templates/';       // temlate directory
+// This file is an organization project and when needed to change the name of folders and whatever.Access is faster and projects more  dynamic, it will make the difference in big projects.
+
+// Routes 
+
+$tpl = 'includes/templates/';       // templates directory
 $css = 'layout/css/';               // css directory
 $js = 'layout/js/';                 // js directory
 $lang 	= 'includes/languages/';    // Language Directory
