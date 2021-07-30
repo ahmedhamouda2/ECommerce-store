21d1a9040840ddfd9a8e9fc7ecfbe59876be22ba
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-07-30 03:23:10 +0300

    create login page and style it

--- admin/index.php
+++ admin/index.php
@@ -4,5 +4,11 @@
     include $lang . 'english.php';
 ?>
 
+<form class="login">
+    <h4 class="text-center">Admin Login</h4>
+    <input class="form-control" type="text" name="user" placeholder="Username" autocomplete="off" />
+    <input class="form-control" type="password" name="pass" placeholder="Password" autocomplete="new-password" />
+    <input class="btn btn-primary btn-block" type="submit" value="Login" />
+</form>
 
 <?php include $tpl . 'footer.php';?>
