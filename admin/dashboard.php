cc178a9a0c1dc7fa15114f4c535e7722dca541a8
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-07-31 23:27:11 +0300

    used the navbar component of bootstrap

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,7 +1,9 @@
 <?php
     session_start();
     if(isset($_SESSION['Username'])){
-        echo 'welcome ' . $_SESSION['Username'];
+        include 'init.php';
+        echo ' welcome';
+        include $tpl . 'footer.php';
     } else {
         header('location:index.php');
         exit();
