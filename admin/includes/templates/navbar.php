19db6ea78ac1457d0838f43e03bbf02aa817b6bf
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-01 02:06:57 +0300

    edit shape navbar in mobile

--- admin/includes/templates/navbar.php
+++ admin/includes/templates/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
+        <a class="navbar-brand active" href="#">Home</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Categories</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Items</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Members</a></li>
