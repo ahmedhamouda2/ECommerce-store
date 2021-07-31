46a644a8246879c55a4dcda554d7a8fcd279a94c
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-01 02:01:13 +0300

    edit navbar and add new links

--- admin/includes/templates/navbar.php
+++ admin/includes/templates/navbar.php
@@ -6,22 +6,14 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Categories</a>
-                </li>
-                
-            </ul>
-        </div>
-
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
+                <li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Categories</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Items</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Members</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Statistics</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Logs</a></li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Ahmed
-                    </a>
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Ahmed </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="#">Edit Profile</a>
                         <a class="dropdown-item" href="#">Settings</a>
