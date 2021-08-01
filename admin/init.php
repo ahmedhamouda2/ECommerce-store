edf024a9678da691f7adac121a1866b0b8397109
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-02 00:32:05 +0300

    calling functions file and check it

--- admin/init.php
+++ admin/init.php
@@ -10,9 +10,11 @@ $tpl = 'includes/templates/';       // templates directory
 $css = 'layout/css/';               // css directory
 $js = 'layout/js/';                 // js directory
 $lang 	= 'includes/languages/';    // Language Directory
+$func 	= 'includes/functions/';    // functions Directory
 
 //include the important files
 
+include $func . 'functions.php';
 include $lang . 'english.php';
 include $tpl . 'header.php';
 
