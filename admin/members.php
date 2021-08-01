2b144be116592f69d283f337a0938ef295001138
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-02 02:41:43 +0300

    divide members page to many pages

--- admin/members.php
+++ admin/members.php
@@ -6,3 +6,26 @@
 	== You Can Add | Edit | Delete Members From Here
 	================================================
 	*/
+
+    session_start();
+    if(isset($_SESSION['Username'])){
+        include 'init.php';
+
+        $do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
+        // start manage page
+        if ($do == 'Manage') {
+            // manage page
+        } elseif ($do == 'Edit') {
+            // Edit page
+            echo 'welcome to edit page';
+        } elseif ($do == 'Insert') {
+            // Insert page
+        } else {
+
+        }
+
+        include $tpl . 'footer.php';
+    } else {
+        header('location:index.php');
+        exit();
+    }
