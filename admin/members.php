3a2aefd62e00372ee1868003c9476437135d3a57
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-02 15:51:49 +0300

    Designing the part for modifying the member's data

--- admin/members.php
+++ admin/members.php
@@ -1,31 +1,77 @@
-<?php 
+<?php
 
-	/*
+/*
 	================================================
 	== Manage Members Page
 	== You Can Add | Edit | Delete Members From Here
 	================================================
 	*/
 
-    session_start();
-    if(isset($_SESSION['Username'])){
-        include 'init.php';
+session_start();
+$pageTitle = 'Members';
+if (isset($_SESSION['Username'])) {
+    include 'init.php';
 
-        $do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
-        // start manage page
-        if ($do == 'Manage') {
-            // manage page
-        } elseif ($do == 'Edit') {
-            // Edit page
-            echo 'welcome to edit page , Your id :' .  $_GET['userid'];
-        } elseif ($do == 'Insert') {
-            // Insert page
-        } else {
+    $do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
+    // start manage page
+    if ($do == 'Manage') {
+        // manage page
+    } elseif ($do == 'Edit') {  // Edit page 
+?>
 
-        }
+        <h2 class="text-center">Edit Member</h2>
+        <div class="container">
+                <form>
+                    <!-- start username field -->
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label d-flex justify-content-sm-end">Username</label>
+                        <div class="col-sm-10 col-md-6">
+                            <input type="username" name="username" class="form-control" autocomplete="off">
+                        </div>
+                    </div>
+                    <!-- End username field -->
+                    <!-- start password field -->
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label d-flex justify-content-sm-end">Password</label>
+                        <div class="col-sm-10 col-md-6">
+                            <input type="password" name="password" class="form-control" autocomplete="new-password">
+                        </div>
+                    </div>
+                    <!-- end password field -->
+                    <!-- start Email field -->
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label d-flex justify-content-sm-end">Email</label>
+                        <div class="col-sm-10 col-md-6">
+                            <input type="email" name="email" class="form-control">
+                        </div>
+                    </div>
+                    <!-- end Email field -->
+                    <!-- start fullname field -->
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label d-flex justify-content-sm-end">Full Name</label>
+                        <div class="col-sm-10 col-md-6">
+                            <input type="text" name="fullname" class="form-control">
+                        </div>
+                    </div>
+                    <!-- end fullname field -->
+                    <!-- start submit -->
+                    <div class="form-group row">
+                        <div class="offset-sm-2 col-sm-10">
+                            <input type="submit" value="save" class="btn btn-primary btn-lg">
+                        </div>
+                    </div>
+                    <!-- end submit -->
+                </form>
+            </form>
+        </div>
 
-        include $tpl . 'footer.php';
+<?php  } elseif ($do == 'Insert') {
+        // Insert page
     } else {
-        header('location:index.php');
-        exit();
     }
+
+    include $tpl . 'footer.php';
+} else {
+    header('location:index.php');
+    exit();
+}
