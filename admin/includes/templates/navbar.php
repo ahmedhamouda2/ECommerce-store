3ac04b1e7e7069b6d5cd89beba83a1fce450acc2
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-04 22:44:46 +0300

    add path for home link

--- admin/includes/templates/navbar.php
+++ admin/includes/templates/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand active" href="#"><?php echo lang('HOME_ADMIN') ?></a>
+        <a class="navbar-brand active" href="dashboard.php"><?php echo lang('HOME_ADMIN') ?></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
