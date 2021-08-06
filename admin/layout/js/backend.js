0a72d71a623893ab196c8d493e18a80bf7fffa74
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-06 16:49:42 +0300

    show category View Option by jquery

--- admin/layout/js/backend.js
+++ admin/layout/js/backend.js
@@ -35,3 +35,16 @@ function hidePassword(){
 $('.confirm').click(function () {
     return confirm('Are You Sure?');
 });
+
+// Category View Option
+$('.cat h3').click(function () {
+    $(this).next('.full-view').fadeToggle(200);
+});
+$('.option span').click(function () {
+    $(this).addClass('active').siblings('span').removeClass('active');
+    if ($(this).data('view') === 'full') {
+        $('.cat .full-view').fadeIn(200);
+    } else {
+        $('.cat .full-view').fadeOut(200);
+    }
+});
