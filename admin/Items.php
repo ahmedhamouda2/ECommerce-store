9da5275646acf76c5f20f39ab56d5d1533a0f248
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-07 16:43:57 +0300

    create Insert item section on database

--- admin/Items.php
+++ admin/Items.php
@@ -24,7 +24,7 @@
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label d-flex justify-content-sm-end">Name</label>
                         <div class="col-sm-10 col-md-6">
-                            <input type="username" name="name" class="form-control" autocomplete="off" required  placeholder="Name of Item">
+                            <input type="username" name="name" class="form-control" autocomplete="off" required placeholder="Name of Item">
                         </div>
                     </div>
                     <!-- End Name field -->
@@ -32,7 +32,7 @@
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label d-flex justify-content-sm-end">Description</label>
                         <div class="col-sm-10 col-md-6">
-                            <input type="username" name="description" class="form-control" autocomplete="off" required  placeholder="Describe the Item">
+                            <input type="username" name="description" class="form-control" autocomplete="off" required placeholder="Describe the Item">
                         </div>
                     </div>
                     <!-- End Description field -->
@@ -40,7 +40,7 @@
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label d-flex justify-content-sm-end">Price</label>
                         <div class="col-sm-10 col-md-6">
-                            <input type="username" name="price" class="form-control" autocomplete="off" required  placeholder="Price the Item">
+                            <input type="username" name="price" class="form-control" autocomplete="off" required placeholder="Price the Item">
                         </div>
                     </div>
                     <!-- End price field -->
@@ -48,7 +48,7 @@
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label d-flex justify-content-sm-end">Country</label>
                         <div class="col-sm-10 col-md-6">
-                            <input type="username" name="country" class="form-control" autocomplete="off" required  placeholder="Country of Made">
+                            <input type="username" name="country" class="form-control" autocomplete="off" required placeholder="Country of Made">
                         </div>
                     </div>
                     <!-- End country field -->
@@ -76,8 +76,64 @@
                 </form>
             </div>
             <?php 
-		} elseif ($do == 'Insert') {
+		} elseif ($do == 'Insert') {  // Insert Items page
 
+        echo "<div class='container'>";
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            echo "<h2 class='text-center'>Insert Items</h2>";
+            echo "<div class='container'>";
+
+            // get variables from the Form [names in form]
+            $name       = $_POST['name'];
+            $desc       = $_POST['description'];
+            $price      = $_POST['price'];
+            $country    = $_POST['country'];
+            $status     = $_POST['status'];
+
+            // validate the Form 
+            $formErrors = array();
+            if(empty($name)){
+                $formErrors[] = 'Name cant be <strong>empty.</strong>';
+            }
+            if(empty($desc)){
+                $formErrors[] = 'Description cant be <strong>empty.</strong>';
+            }
+            if(empty($price)){
+                $formErrors[] = 'Price cant be <strong>empty.</strong>';
+            }
+            if(empty($country)){
+                $formErrors[] = 'Country cant be <strong>empty.</strong>';
+            }
+            if($status == 0){
+                $formErrors[] = 'You must choose the <strong>Status.</strong>';
+            }
+            // loop into errors array and echo it 
+            foreach($formErrors as $error){
+                echo  '<div class="alert alert-danger" role="alert">' . $error . '</div>';
+            }
+
+            // check if there no error proceed the update operation
+                if(empty($formErrors)){
+                    // Insert user info in database
+                    $stmt = $con->prepare("INSERT INTO items(`Name` , `Description` , Price , Country_Made ,`Status` , Add_Date) VALUES(:zname , :zdesc , :zprice , :zcountry , :zstatus , now())");
+                    $stmt->execute(array(
+                        'zname' => $name,
+                        'zdesc' => $desc,
+                        'zprice' => $price,
+                        'zcountry' => $country,
+                        'zstatus' => $status,
+                    ));
+    
+                    // echo success message 
+                    $theMsg = "<div class='alert alert-success' role='alert'>" . $stmt->rowCount() . ' Record Inserted</div>';
+                    redirectHome($theMsg, 'back');
+                }
+
+        } else {
+            $theMsg = "<div class='alert alert-danger' role='alert'>Sorry you cant browse this page directly</div>";
+            redirectHome($theMsg);
+        }
+        echo "</div>";
 
 		} elseif ($do == 'Edit') {
 
