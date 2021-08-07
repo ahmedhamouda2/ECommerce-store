6e43afa84c1493434e5762d2677a4aa98c33c0b5
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-07 15:06:22 +0300

    create Items page

---
+++ admin/Items.php
@@ -0,0 +1,48 @@
+<?php
+
+	/*
+	================================================
+	== Items Page
+	================================================
+	*/
+
+	session_start();
+	$pageTitle = 'Items';
+	if (isset($_SESSION['Username'])) {
+		include 'init.php';
+		$do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
+		if ($do == 'Manage') {
+
+            echo 'Welcome to Items page';
+
+		} elseif ($do == 'Add') {
+
+
+		} elseif ($do == 'Insert') {
+
+
+		} elseif ($do == 'Edit') {
+
+
+		} elseif ($do == 'Update') {
+
+
+		} elseif ($do == 'Delete') {
+
+
+		} elseif ($do == 'Approve') {
+
+
+		}
+
+		include $tpl . 'footer.php';
+
+	} else {
+
+		header('Location: index.php');
+
+		exit();
+	}
+
+
+?>
