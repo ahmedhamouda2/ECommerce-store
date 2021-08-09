ffd0e4711a7bdbf32e41722cb82c4bbb9f3e16ef
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-09 23:42:02 +0300

    get categories from admin categories page and show this in header with dynamic method

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -15,26 +15,19 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand active" href="dashboard.php"><?php echo lang('HOME_ADMIN') ?></a>
+            <a class="navbar-brand active" href="index.php">HomePage</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link" href="categories.php"><?php echo lang('CATEGORIES') ?></a></li>
-                    <li class="nav-item"><a class="nav-link" href="Items.php"><?php echo lang('ITEMS') ?></a></li>
-                    <li class="nav-item"><a class="nav-link" href="members.php"><?php echo lang('MEMBERS') ?></a></li>
-                    <li class="nav-item"><a class="nav-link" href="comments.php"><?php echo lang('COMMENTS') ?></a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo lang('DROPDOWN_Name') ?></a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="members.php?do=Edit&userid=<?php echo $_SESSION['ID'] ?>"><?php echo lang('EDIT_PROFILE') ?></a>
-                            <a class="dropdown-item" href="#"><?php echo lang('SETTINGS') ?></a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="logout.php"><?php echo lang('LOGOUT') ?></a>
-                        </div>
-                    </li>
+                <ul class="navbar-nav ml-auto">
+                    <?php
+                        $categories = getCat();
+                        foreach($categories as $cat) {
+                            echo '<li class="nav-item"><a class="nav-link" href="#">' . $cat['Name'] . '</a></li>';
+                        }
+                    ?>
                 </ul>
             </div>
         </div>
