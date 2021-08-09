b852e9aba95b37f8cfff37730f781daf94d542f5
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-09 22:48:41 +0300

    add link for shop side within admin side and ignore links not used at now time

--- admin/includes/templates/navbar.php
+++ admin/includes/templates/navbar.php
@@ -11,11 +11,10 @@
                 <li class="nav-item"><a class="nav-link" href="Items.php"><?php echo lang('ITEMS') ?></a></li>
                 <li class="nav-item"><a class="nav-link" href="members.php"><?php echo lang('MEMBERS') ?></a></li>
                 <li class="nav-item"><a class="nav-link" href="comments.php"><?php echo lang('COMMENTS') ?></a></li>
-                <li class="nav-item"><a class="nav-link" href="#"><?php echo lang('STATISTICS') ?></a></li>
-                <li class="nav-item"><a class="nav-link" href="#"><?php echo lang('LOGS') ?></a></li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo lang('DROPDOWN_Name') ?></a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="../index.php" target="_blank">Visit Shop</a>
                         <a class="dropdown-item" href="members.php?do=Edit&userid=<?php echo $_SESSION['ID'] ?>"><?php echo lang('EDIT_PROFILE') ?></a>
                         <a class="dropdown-item" href="#"><?php echo lang('SETTINGS') ?></a>
                         <div class="dropdown-divider"></div>
