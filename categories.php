520a6d49cacc6f5a1ff91c244addd4f3f60eb6dc
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-10 11:18:13 +0300

    create structure of categories page and create a frontend CSS file

--- categories.php
+++ categories.php
@@ -2,11 +2,21 @@
 
     <div class="container">
         <h2 class="text-center"><?php echo str_replace('-' , ' ' , $_GET['pagename']) ?></h2>
-        <?php
-            foreach(getitems($_GET['pageid']) as $item){
-                echo $item['Name'];
-            }
-        ?>
+        <div class="row">
+            <?php
+                foreach(getitems($_GET['pageid']) as $item){
+                    echo '<div class="col-sm-6 col-md-3">';
+                        echo '<div class="thumbnail item-box">';
+                            echo '<img class="img-responsive" src="https://picsum.photos/250" alt="">';
+                            echo '<div class="caption">';
+                                echo '<h3>' . $item['Name'] . '</h3>';
+                                echo '<p>' . $item['Description'] . '</p>';
+                            echo '</div>';
+                        echo '</div>';
+                    echo '</div>';
+                }
+            ?>
+        </div>
     </div>
 
 <?php include $tpl . 'footer.php';?>
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="<?php echo $css?>jquery-ui.min.css">
         <link rel="stylesheet" href="<?php echo $css?>jquery.selectBoxIt.css">
-        <link rel="stylesheet" href="<?php echo $css?>backend.css">
+        <link rel="stylesheet" href="<?php echo $css?>frontend.css">
     </head>
     <body>
 
