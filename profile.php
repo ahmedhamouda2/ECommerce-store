acf983b09ab3c8a55de247df6bc3916f7930e9a7
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-11 16:34:51 +0300

    create layout and style for profile page

--- profile.php
+++ profile.php
@@ -2,6 +2,43 @@
     session_start();
     $pageTitle = 'Profile';
     include 'init.php';
-    echo 'welcome ' . $_SESSION['user'];
-    include $tpl . 'footer.php';
 ?>
+    <h2 class="text-center">My Profile</h2>
+    <div class="information block">
+        <div class="container">
+            <div class="card">
+                <div class="card-header bg-primary text-white">
+                    My Information
+                </div>
+                <div class="card-body">
+                    Name : Ahmed
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="my-ads block">
+        <div class="container">
+            <div class="card">
+                <div class="card-header bg-primary text-white">
+                    My-Ads
+                </div>
+                <div class="card-body">
+                    Test Ads
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="my-comments block">
+        <div class="container">
+            <div class="card">
+                <div class="card-header bg-primary text-white">
+                    Latest Comments
+                </div>
+                <div class="card-body">
+                    Test Comments
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include $tpl . 'footer.php';?>
