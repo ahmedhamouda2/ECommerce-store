f3a242cb745c8f398aa6727d4179338bc7f35afe
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-11 22:24:05 +0300

    get user ads and show this in profile page

--- profile.php
+++ profile.php
@@ -30,7 +30,25 @@
                     My-Ads
                 </div>
                 <div class="card-body">
-                    Test Ads
+                    <div class="row">
+                        <?php
+                            foreach(getitems('Member_ID' , $info['UserID']) as $item){
+                                echo '<div class="col-sm-6 col-md-4 col-lg-3">';
+                                    echo '<div class="card">';
+                                        echo '<img class="card-img-top img-fluid img-thumbnail" src="https://picsum.photos/250" alt="Card image">';
+                                        echo '<div class="card-body border-0">';
+                                            echo '<h4 class="card-title">' . $item['Name'] . '</h4>';
+                                            echo '<p class="card-text">' . $item['Description'] . '</p>';
+                                            echo '<div class="d-flex justify-content-around">';
+                                                echo '<button type="button" class="btn btn-light">' . $item['Price'] . '</button>'; 
+                                                echo '<a href="#" class="btn btn-primary">See More</a>';
+                                            echo '</div>';
+                                        echo '</div>';
+                                    echo '</div>';
+                                echo '</div>';
+                            }
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
