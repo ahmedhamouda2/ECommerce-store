8db72bfd69549afc9dafbf10ad1d706d58bea4ed
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-14 00:20:12 +0300

    fix problem repeat comment when refresh page

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -8,5 +8,10 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="<?php echo $js?>jquery.selectBoxIt.min.js"></script>
         <script src="<?php echo $js?>frontend.js"></script>
+        <script>
+            if (window.history.replaceState) {
+                window.history.replaceState( null, null, window.location.href );
+            }     
+        </script>
     </body>
 </html>
