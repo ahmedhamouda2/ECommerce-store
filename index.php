e4a69dc0a211abdcc0c0790d32142257121dd4d3
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-14 15:45:08 +0300

    show all items in home page

--- index.php
+++ index.php
@@ -2,7 +2,27 @@
     session_start();
     $pageTitle = 'HomePage';
     include 'init.php';
-
+?>
+    <div class="container">
+        <div class="row">
+            <?php
+            $allItems = getAllFrom('items', 'item_ID');
+                foreach($allItems as $item){
+                    echo '<div class="col-sm-6 col-md-4 col-lg-3">';
+                        echo '<div class="card">';
+                        echo '<span class="price">$' . $item['Price'] . '</span>';
+                            echo '<img class="card-img-top img-fluid img-thumbnail" src="https://picsum.photos/250" alt="Card image">';
+                            echo '<div class="card-body">';
+                                echo '<h4 class="card-title"><a href="items.php?itemid=' . $item['Item_ID'] . '">' . $item['Name'] . '</a></h4>';
+                                echo '<p class="card-text">' . $item['Description'] . '</p>';
+                                echo '<div class="date text-right">' . $item['Add_Date'] . '</div>';
+                            echo '</div>';
+                        echo '</div>';
+                    echo '</div>';
+                }
+            ?>
+        </div>
+    </div>
+<?php 
     include $tpl . 'footer.php';
-
 ?>
