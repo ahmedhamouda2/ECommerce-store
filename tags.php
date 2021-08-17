ee5b691ea704ee117d601e0f1fed256f666f83bf
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-17 17:29:48 +0300

    filter and find items by tag name

--- tags.php
+++ tags.php
@@ -4,12 +4,30 @@
 ?>
 
     <div class="container">
-        <div class="row justify-content-center">
-            <?php
-                $tag = isset($_GET['name']) ? $_GET['name']: 0;
-                echo '<h2>' . $_GET['name'] . '</h2>';
+            <?php 
+            if(isset($_GET['name'])) {
+                $tag = $_GET['name'];
+                echo '<h2 class="text-center">' . $tag . '</h2>';
+                $tagsItems = getAllFrom("*","items","WHERE tags LIKE '%$tag%'" , "AND Approve = 1" , "Item_ID");
+                echo '<div class="row">';
+                    foreach($tagsItems as $item){
+                        echo '<div class="col-sm-6 col-md-4 col-lg-3">';
+                            echo '<div class="card mt-3">';
+                            echo '<span class="price">$' . $item['Price'] . '</span>';
+                                echo '<img class="card-img-top img-fluid img-thumbnail" src="https://picsum.photos/250" alt="Card image">';
+                                echo '<div class="card-body">';
+                                    echo '<h4 class="card-title"><a href="items.php?itemid=' . $item['Item_ID'] . '">' . $item['Name'] . '</a></h4>';
+                                    echo '<p class="card-text">' . $item['Description'] . '</p>';
+                                    echo '<div class="date text-right">' . $item['Add_Date'] . '</div>';
+                                echo '</div>';
+                            echo '</div>';
+                        echo '</div>';
+                    }
+                echo '</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">You must enter <strong>Tag name</strong></div>';
+            }
             ?>
-        </div>
     </div>
 
 <?php include $tpl . 'footer.php';?>
