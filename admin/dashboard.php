7768ef6c61b5bc569f5141a1bc282a9cead23dae
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-18 17:51:52 +0300

    general fix of design the dashboard page

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -21,7 +21,7 @@
                                 Total Members 
                                 <span><?php echo countItems('UserID', 'users') ?></span>
                             </div>
-                            <div><i class="fas fa-users fa-5x"></i></div>
+                            <div><i class="fas fa-users fa-5x  fa-fw"></i></div>
                         </div>
                     </a>
                 </div>
@@ -32,7 +32,7 @@
                                 Pending Members 
                                 <span><?php echo checkItem('RegStatus', 'users', 0) ?></span>
                             </div>
-                            <div><i class="fas fa-user-clock fa-5x"></i></i></div>
+                            <div><i class="fas fa-user-clock fa-5x fa-fw"></i></i></div>
                         </div>
                     </a>
                 </div>
@@ -43,7 +43,7 @@
                                 Total Items 
                                 <span><?php echo countItems('Item_ID', 'items') ?></span>
                             </div>
-                            <div><i class="fas fa-tags fa-5x"></i></div>
+                            <div><i class="fas fa-tags fa-5x fa-fw"></i></div>
                         </div>
                     </a>
                 </div>
@@ -54,7 +54,7 @@
                                 Total Comments 
                                 <span><?php echo countItems('c_id', 'comments') ?></span>
                             </div>
-                            <div><i class="fas fa-comments fa-5x"></i></div>
+                            <div><i class="fas fa-comments fa-5x fa-fw"></i></div>
                         </div>
                     </a>
                 </div>
@@ -64,11 +64,11 @@
         <section class="container latest">
             <div class="row">
                 <div class="col-sm-6">
-                    <div class="card">
+                    <div class="card mb-3">
                         <div class="card-header">
-                            <i class="fa fa-users"></i> Latest <?php echo $latestNumUsers ?> Registered Users 
+                            <i class="fa fa-users fa-fw color-icon"></i> Latest <?php echo $latestNumUsers ?> Registered Users 
                             <span class="float-right toggle-info">
-                                <i class="fa fa-plus"></i>
+                                <i class="fa fa-plus fa-fw"></i>
                             </span>
                         </div>
                         <div class="card-body">
@@ -77,12 +77,17 @@
                                     if(!empty($latestNumUsers)){
                                         foreach($theLatestUsers as $user){
                                             echo '<li>';
+                                                if(empty($user['Avatar'])){
+                                                    echo '<img class="rounded-circle" src="uploads/avatars/default.png" alt = "Default image">';
+                                                } else {
+                                                    echo '<img class="rounded-circle" src="uploads/avatars/' . $user['Avatar'] . '" alt = "avatar image">';
+                                                }
                                                 echo $user['Username'];
                                                 echo '<a href="members.php?do=Edit&userid=' . $user['UserID'] . '">';
                                                     echo '<span class="btn btn-success float-right">';
-                                                        echo '<i class="fa fa-edit"></i> Edit';
+                                                        echo '<i class="fa fa-edit  fa-fw"></i> Edit';
                                                         if($user['RegStatus'] == 0){
-                                                            echo '<a href="members.php?do=Activate&userid=' . $user['UserID'] . '" class="btn btn-info activate float-right"><i class="fas fa-user-check"></i> Activate</a>';
+                                                            echo '<a href="members.php?do=Activate&userid=' . $user['UserID'] . '" class="btn btn-info activate float-right"><i class="fas fa-user-check  fa-fw"></i> Activate</a>';
                                                         }
                                                     echo '</span>';
                                                 echo '</a>';
@@ -99,9 +104,9 @@
                 <div class="col-sm-6">
                     <div class="card">
                         <div class="card-header">
-                            <i class="fa fa-tag"></i> Latest Items
+                            <i class="fa fa-tag fa-fw color-icon"></i> Latest Items
                             <span class="float-right toggle-info">
-                                <i class="fa fa-plus"></i>
+                                <i class="fa fa-plus fa-fw"></i>
                             </span>
                         </div>
                         <div class="card-body">
@@ -113,9 +118,9 @@
                                                 echo $item['Name'];
                                                 echo '<a href="Items.php?do=Edit&itemid=' . $item['Item_ID'] . '">';
                                                     echo '<span class="btn btn-success float-right">';
-                                                        echo '<i class="fa fa-edit"></i> Edit';
+                                                        echo '<i class="fa fa-edit fa-fw"></i> Edit';
                                                         if($item['Approve'] == 0){
-                                                            echo '<a href="Items.php?do=Approve&itemid=' . $item['Item_ID'] . '" class="btn btn-info approve float-right"><i class="fas fa-check"></i> Approve</a>';
+                                                            echo '<a href="Items.php?do=Approve&itemid=' . $item['Item_ID'] . '" class="btn btn-info approve float-right"><i class="fas fa-check fa-fw"></i> Approve</a>';
                                                         }
                                                     echo '</span>';
                                                 echo '</a>';
@@ -133,18 +138,17 @@
             <!-- start latest comment -->
             <div class="row">
                 <div class="col-sm-6">
-                    <div class="card">
+                    <div class="card mb-3">
                         <div class="card-header">
-                            <i class="fa fa-comments"></i> Latest Comments 
+                            <i class="fa fa-comments fa-fw color-icon"></i> Latest Comments 
                             <span class="float-right toggle-info">
-                                <i class="fa fa-plus"></i>
+                                <i class="fa fa-plus fa-fw"></i>
                             </span>
                         </div>
                         <div class="card-body">
                             <?php 
-                                // select all users except Admin
                                 $stmt = $con->prepare("SELECT 
-                                                            comments.* , users.Username 
+                                                            comments.* , users.Username , users.Avatar
                                                         FROM 
                                                             comments 
                                                         INNER JOIN
@@ -158,7 +162,14 @@
                                 if(!empty($comments)) {
                                     foreach($comments as $comment) {
                                         echo '<div class="comment-box d-flex">';
-                                            echo '<span class="user-n">' . $comment['Username'] . '</span>';
+                                            echo '<div class="user-n">';
+                                                if(empty($comment['Avatar'])){
+                                                    echo '<img class="rounded-circle" src="uploads/avatars/default.png" alt = "Default image">';
+                                                } else {
+                                                    echo '<img class="rounded-circle" src="uploads/avatars/' . $comment['Avatar'] . '" alt = "avatar image">';
+                                                }
+                                                echo '<span>' . $comment['Username'] . '</span>';
+                                            echo '</div>';
                                             echo '<p class="user-c">' . $comment['comment'] . '</p>';
                                         echo '</div>';
                                     }
--- admin/includes/templates/navbar.php
+++ admin/includes/templates/navbar.php
@@ -11,17 +11,17 @@
                 <li class="nav-item"><a class="nav-link" href="Items.php"><?php echo lang('ITEMS') ?></a></li>
                 <li class="nav-item"><a class="nav-link" href="members.php"><?php echo lang('MEMBERS') ?></a></li>
                 <li class="nav-item"><a class="nav-link" href="comments.php"><?php echo lang('COMMENTS') ?></a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo lang('DROPDOWN_Name') ?></a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="../index.php" target="_blank"><i class="fas fa-shopping-cart fa-fw"></i> <?php echo lang('Visit_Shop') ?></a>
-                        <a class="dropdown-item" href="members.php?do=Edit&userid=<?php echo $_SESSION['ID'] ?>"><i class="fas fa-user fa-fw"></i> <?php echo lang('EDIT_PROFILE') ?></a>
-                        <a class="dropdown-item" href="#"><i class="fas fa-cog fa-fw"></i> <?php echo lang('SETTINGS') ?></a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="logout.php"><i class="fas fa-sign-out-alt fa-fw"></i> <?php echo lang('LOGOUT') ?></a>
-                    </div>
-                </li>
             </ul>
+            <div class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo lang('DROPDOWN_Name') ?></a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="../index.php" target="_blank"><i class="fas fa-shopping-cart fa-fw"></i> <?php echo lang('Visit_Shop') ?></a>
+                    <a class="dropdown-item" href="members.php?do=Edit&userid=<?php echo $_SESSION['ID'] ?>"><i class="fas fa-user fa-fw"></i> <?php echo lang('EDIT_PROFILE') ?></a>
+                    <a class="dropdown-item" href="#"><i class="fas fa-cog fa-fw"></i> <?php echo lang('SETTINGS') ?></a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="logout.php"><i class="fas fa-sign-out-alt fa-fw"></i> <?php echo lang('LOGOUT') ?></a>
+                </div>
+            </div>
         </div>
     </div>
 </nav>
