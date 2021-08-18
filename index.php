0fe7c60e6c307c0885f249ea847a628d69057ffe
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-08-19 00:43:23 +0300

    general fix of design frontend

--- index.php
+++ index.php
@@ -10,14 +10,19 @@
                 foreach($allItems as $item){
                     echo '<div class="col-sm-6 col-md-4 col-lg-3">';
                         echo '<div class="card mt-3">';
-                        echo '<span class="price">$' . $item['Price'] . '</span>';
-                            echo '<img class="card-img-top img-fluid img-thumbnail" src="https://picsum.photos/250" alt="Card image">';
-                            echo '<div class="card-body">';
-                                echo '<h4 class="card-title"><a href="items.php?itemid=' . $item['Item_ID'] . '">' . $item['Name'] . '</a></h4>';
-                                echo '<p class="card-text">' . $item['Description'] . '</p>';
-                                echo '<div class="date text-right">' . $item['Add_Date'] . '</div>';
-                            echo '</div>';
-                        echo '</div>';
+                            echo '<a class="custom-item" href="items.php?itemid=' . $item['Item_ID'] . '">';
+                                echo '<span class="price">$' . $item['Price'] . '</span>';
+                                    echo '<img class="card-img-top img-fluid img-thumbnail" src="https://picsum.photos/250" alt="Card image">';
+                                    echo '<div class="card-body">';
+                                        echo '<h4 class="card-title">' . $item['Name'] . '</h4>';
+                                        echo '<p class="card-text">' . $item['Description'] . '</p>';
+                                        echo '<div class="d-flex justify-content-between">';
+                                            echo '<span class="date text-left">' . $item['Country_Made'] . '</span>';
+                                            echo '<span class="date text-right">' . $item['Add_Date'] . '</span>';
+                                        echo '</div>';
+                                    echo '</div>';
+                                echo '</div>';
+                            echo '</a>';
                     echo '</div>';
                 }
             ?>
--- profile.php
+++ profile.php
@@ -55,14 +55,19 @@
                         foreach($myItems as $item){
                             echo '<div class="col-sm-6 col-md-4 col-lg-3">';
                                 echo '<div class="card mt-3">';
-                                    if($item['Approve'] == 0) {echo '<span class="approve-msg">Awaiting Approval</span>';}
-                                    echo '<span class="price">$' . $item['Price'] . '</span>'; 
-                                    echo '<img class="card-img-top img-fluid img-thumbnail" src="https://picsum.photos/250" alt="Card image">';
-                                    echo '<div class="card-body border-0">';
-                                        echo '<h4 class="card-title"><a href="items.php?itemid=' . $item['Item_ID'] . '">' . $item['Name'] . '</a></h4>';
-                                        echo '<p class="card-text">' . $item['Description'] . '</p>';
-                                        echo '<div class="date text-right">' . $item['Add_Date'] . '</div>';
-                                    echo '</div>';
+                                    echo '<a class="custom-item" href="items.php?itemid=' . $item['Item_ID'] . '">';
+                                        if($item['Approve'] == 0) {echo '<span class="approve-msg">Awaiting Approval</span>';}
+                                        echo '<span class="price">$' . $item['Price'] . '</span>'; 
+                                        echo '<img class="card-img-top img-fluid img-thumbnail" src="https://picsum.photos/250" alt="Card image">';
+                                        echo '<div class="card-body border-0">';
+                                            echo '<h4 class="card-title">' . $item['Name'] . '</h4>';
+                                            echo '<p class="card-text">' . $item['Description'] . '</p>';
+                                            echo '<div class="d-flex justify-content-between">';
+                                                echo '<span class="date text-left">' . $item['Country_Made'] . '</span>';
+                                                echo '<span class="date text-right">' . $item['Add_Date'] . '</span>';
+                                            echo '</div>';
+                                        echo '</div>';
+                                    echo '</a>';
                                 echo '</div>';
                             echo '</div>';
                         }
